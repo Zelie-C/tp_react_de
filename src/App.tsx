2f9dbee58d4fec0ca81a8efc28091be00a393689
Zelie-C <zelie.chemin@gmail.com>
2023-11-06 15:20:14 +0100

    fin exo 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,73 @@
 
-import { useCallback, useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import Die from "./Die"
 
 function App() {
   
-const [newValue, setNewValue] = useState(1)
+  // const [value1, setValue1] = useState(Math.floor(Math.random() * 7))
+  // const [value2, setValue2] = useState(Math.floor(Math.random() * 7))
+  // const [value3, setValue3] = useState(Math.floor(Math.random() * 7))
+  
+ 
+
+  // const handleRoll1 = useCallback(
+  //   (dieValue: number, dieIndex) => {
+  //     values[dieIndex] = 
+  //     setValue1(dieValue)
+  //   }, []
+  // )
+  // const handleRoll2 = useCallback(
+  //   (dieValue: number) => {
+  //     setValue2(dieValue)
+  //   }, []
+  // )
+  // const handleRoll3 = useCallback(
+  //   (dieValue: number) => {
+  //     setValue3(dieValue)
+  //   }, []
+  // )
+  const [values, setValues] = useState([1, 5, 6, 8])
+
+   const sumDices = useMemo(() => {
+    return values.reduce((acc, curV) => acc + curV, 0);
+  }, [values]
+  )
 
   const handleRoll = useCallback(
-    (dieValue: number) => {
-      setNewValue(dieValue)
-    }, [newValue]
-  )
+    (dieValue: number, dieIndex: number) => {
+      setValues(prev => {
+        const newValues = [...prev];
+        newValues[dieIndex] = dieValue
+        return newValues
+      })
+  }, [])
 
   return (
     <div>
-      <Die onRoll={handleRoll}/>
-      <p>{newValue}</p>
+      {values.map((value, index) => (
+        <>
+        <Die key={index} value={value} onRoll={(dieValue)=> {handleRoll(dieValue, index)}}  /> 
+        <p>{value}</p> 
+        </>
+      ))}
+      <p>{sumDices}</p>
     </div>
   )
-}
+  // return (
+  //   <>
+  //   <div>
 
-export default App
+  //     {values.map((value, index) => (
+  //       <Die key={index} value={value} onRoll={handleRoll}>
+  //     ))}
+      
+  //   </div>
+  //   </>
+      
+  // )
+}
+{/* <Die value={values[]} onRoll={handleRoll1}/>
+<Die value={values[]} onRoll={handleRoll2}/>
+<Die value={value3} onRoll={handleRoll3}/>
+<p>{sumDices}</p> */}
+export default App;
--- src/Die.tsx
+++ src/Die.tsx
@@ -1,18 +1,21 @@
-import { useCallback, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 
-const Die = (props: {onRoll: (newVal: number) => void}) => {
+const Die = (props: {value: number, onRoll: (newVal: number) => void}) => {
 
     const [value, setValue] = useState(Math.floor(Math.random() * 7));
 
+    useEffect (() => {
+        props.onRoll(Math.floor(Math.random() * 7))
+    }, [value])
+
     const handleClick = useCallback(() => {
         const rand = Math.floor(Math.random() * 7)
-        props.onRoll(rand)
         setValue(rand)
     }, [])
 
     return (
-        <div onClick={handleClick}>{value}</div>
+        <div onClick={handleClick}>{props.value}</div>
     )
 }
 
